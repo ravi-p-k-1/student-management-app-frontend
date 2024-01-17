dcf476fbbd13be741bf3d4863b9891cd39a1de09
Ravi <ravipkakadia1@gmail.com>
2024-01-17 10:56:22 +0530

    View Student Data done

--- src/Components/View.tsx
+++ src/Components/View.tsx
@@ -1,63 +1,60 @@
-import React, { useEffect, useState } from 'react'
-import { ViewTableDataInterface } from '../Interfaces/ViewTableDataInterface';
-import { ViewStudentServiceAPI } from '../Services';
+import React, { useEffect, useState } from "react";
+import { ViewTableDataInterface } from "../Interfaces/ViewTableDataInterface";
+import { ViewStudentServiceAPI } from "../Services";
+import { AxiosResponse } from "axios";
+import { ToastContainer, toast } from "react-toastify";
 
 export const View = () => {
+  const [tableData, setTableData] = useState<ViewTableDataInterface[] | []>([]);
 
-    const [tableData, setTableData] = useState< ViewTableDataInterface[] | [] >([]);
-
-    useEffect(() => {
-      const fetchData=async()=>{
-        const temp = await ViewStudentServiceAPI();
-        setTableData(temp.data);
-        localStorage.setItem("studentData", JSON.stringify(temp.data));
+  useEffect(() => {
+    const fetchData = async () => {
+      const resp:AxiosResponse = await ViewStudentServiceAPI();
+      if (resp && resp.data) {
+        setTableData(resp.data);
+        localStorage.setItem("studentData", JSON.stringify(resp.data));
+        toast.success("Data Fetched Succesfully", {
+            position: "top-right",
+        });
       }
+    };
 
-      var data = localStorage.getItem("studentData");
+    var data = localStorage.getItem("studentData");
 
-      if(data===null){
-        fetchData();
-      } else{
-        setTableData(JSON.parse(data));
-      }
-    }, []);
+    if (data === null) {
+      fetchData();
+    } else {
+      setTableData(JSON.parse(data));
+    }
+  }, []);
 
   return (
-    <div className='view'>
-        <table id='viewtable'>
-            <thead>
-                <tr>
-                    <th>ID</th>
-                    <th>Name</th>
-                    <th>Email</th>
-                    <th>Age</th>
-                    <th>DOB</th>
-                </tr>
-            </thead>
-            <tbody>
-                {
-                    tableData.map((data, index)=>{
-                        return <tr key={index}>
-                            <td>
-                                {data.id}
-                            </td>
-                            <td>
-                                {data.name}
-                            </td>
-                            <td>
-                                {data.email}
-                            </td>
-                            <td>
-                                {data.age}
-                            </td>
-                            <td>
-                                {data.dob}
-                            </td>
-                        </tr>
-                    })
-                }
-            </tbody>
-        </table>
+    <div className="view">
+      <table id="viewtable">
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Name</th>
+            <th>Email</th>
+            <th>Age</th>
+            <th>DOB</th>
+          </tr>
+        </thead>
+        <tbody>
+          {tableData.length>0 ? tableData.map((data, index) => {
+            return (
+              <tr key={index}>
+                <td>{data.id}</td>
+                <td>{data.name}</td>
+                <td>{data.email}</td>
+                <td>{data.age}</td>
+                <td>{data.dob}</td>
+              </tr>
+            );
+          }): <tr><td colSpan={5}>No data to show</td></tr>}
+        </tbody>
+      </table>
+      <ToastContainer />
     </div>
-  )
-}
+  );
+};
--- src/Services.tsx
+++ src/Services.tsx
@@ -1,9 +1,42 @@
-import axios from 'axios'
+import axios, { AxiosError, AxiosRequestConfig, AxiosResponse } from "axios";
+import { toast } from "react-toastify";
 
 const baseURL = "http://localhost:4000";
 
-export const ViewStudentServiceAPI=async()=>{
-    return await axios.get(`${baseURL}/api/v1/students`);
-}
+const instance = axios.create({
+  baseURL: baseURL,
+  timeout: 1000,
+});
 
+instance.interceptors.request.use(
+  (config) => {
+    return config;
+  },
+  (error:AxiosError) => {
+    return error;
+  }
+);
 
+instance.interceptors.response.use(
+  (response:AxiosResponse) => {
+    return response;
+  },
+  (error: AxiosError) => {
+    console.log("this is my log",error);
+    if(error.code && error.code==="ECONNABORTED"){
+        toast.error(error.message, {
+            position: "top-right",
+        });
+    }
+    if(error.code && error.code==="ERR_NETWORK"){
+        toast.error(error.message, {
+            position: "top-right",
+        });
+    }
+    return error;
+  }
+);
+
+export const ViewStudentServiceAPI = async () => {
+  return await instance.get(`${baseURL}/api/v1/students`);
+};
